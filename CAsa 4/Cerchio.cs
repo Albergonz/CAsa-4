35f670c253c4a6d3d903c64226d6c9d4e2e3c542
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-23 18:17:33 +0100

    Aggiungere i file di progetto.

---
+++ CAsa 4/Cerchio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAsa_4
+{
+    internal class Cerchio
+    {
+        public int Raggio { get; set; }
+        public double Area()
+        {
+            return Raggio * Raggio * Math.PI;
+        }
+
+        public double Circonferenza()
+        { 
+        return 2* Raggio * Math.PI;
+        }
+    }
+}
---
+++ CAsa 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAsa_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio cerchio = new Cerchio();
+            cerchio.Raggio = int.Parse(Console.ReadLine());
+            Console.WriteLine("A:{0}", cerchio.Area());
+            Console.WriteLine("A:{0}", cerchio.Circonferenza());
+
+
+
+        }
+    }
+}
