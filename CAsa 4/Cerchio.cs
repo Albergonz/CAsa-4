ee23353ed13b97ce1b32d5c52d31a157198860f1
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-25 17:02:05 +0100

    + override

--- CAsa 4/Cerchio.cs
+++ CAsa 4/Cerchio.cs
@@ -18,5 +18,9 @@ namespace CAsa_4
         { 
         return 2* Raggio * Math.PI;
         }
+
+        public override string ToString() {
+            return string.Format("Cerchio di raggio {0}", Raggio);
+        }
     }
 }
